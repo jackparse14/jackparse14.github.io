723f0ba91d75aab19adc11afaa801cdd9943e014
Jack Parsons <jackparse14@hotmail.co.uk>
2023-11-06 15:23:00 +0000

    Added Game Canvas

--- src/js/game/game.js
+++ src/js/game/game.js
@@ -1,1 +1,8 @@
-console.log("Hello World! - GAME")
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20,40,50,50);
+ctx.fillstyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
